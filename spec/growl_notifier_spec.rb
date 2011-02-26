8ea395d8931856c3fd5926ab4986ce06061e7bcf
Amir Rajan <ar@amirrajan.net>
2011-02-26 13:25:19 -0600

    nuget package

--- spec/growl_notifier_spec.rb
+++ spec/growl_notifier_spec.rb
@@ -1,5 +1,19 @@
 require "./watcher_dot_net.rb"
 
 describe GrowlNotifier do
+  before(:each) { @array = Array.new }
 
+  context "something that works in rspec but not nspec" do
+    context "sibling context" do
+      before(:each) { @array << "sibling 1" }
+
+      it { @array.count.should == 1 }
+    end
+
+    context "another sibling context" do
+      before(:each) { @array << "sibling 2" }
+
+      it { @array.count.should == 1 }
+    end
+  end
 end
