b8bf48429309823134a8e96d263f6842f4ae94ff
Amir Rajan <ar@amirrajan.net>
2011-03-22 19:56:52 -0500

    updated nuget package

--- init.ps1
+++ init.ps1
@@ -8,9 +8,17 @@ $configFileTo = "dotnet.watchr.rb"
 $watchrFileFrom = $toolsPath + "\watcher_dot_net.rb"
 $watchrFileTo = "watcher_dot_net.rb"
 
+$redFileFrom = $toolsPath + "\watcher_dot_net.rb"
+$redFileTo = "red.png"
+
+$greenFileFrom = $toolsPath + "\watcher_dot_net.rb"
+$greenFileTo = "green.png"
+
 if(!(Test-Path $configFileTo))
 {
   Copy-Item $configFileFrom $configFileTo
   Copy-Item $watchrFileFrom $watchrFileTo
+  Copy-Item $redFileFrom $redFileTo
+  Copy-Item $greenFileFrom $greenFileTo
   Start-Process $usageFile
 }
